dc404db2d590c6790636203be0aab9d2a12721e6
Nuno Freittas <nuno.freitas96@gmail.com>
2021-08-12 19:05:06 +0100

    Start of Client Search Component

--- client/src/app/client-search/client-search.component.ts
+++ client/src/app/client-search/client-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-client-search',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-search.component.css']
 })
 export class ClientSearchComponent implements OnInit {
+  clientResults = [];
+  constructor(
+    private http: HttpClient) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.http.get<any>('http://localhost:8080/clients').subscribe(data => {
+      this.clientResults = data;
+      console.log(this.clientResults);
+    })
   }
-
 }
