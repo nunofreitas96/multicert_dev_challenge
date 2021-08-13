3db39c9c0c2e0e7b3bdd16f318dd4b3a2ec47859
Nuno Freittas <nuno.freitas96@gmail.com>
2021-08-13 09:33:02 +0100

    Client Finished

--- client/src/app/create-client/create-client.component.ts
+++ client/src/app/create-client/create-client.component.ts
@@ -1,27 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import {Model} from "../client-search/client-search.component";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    Authorization: 'my-auth-token'
+  })};
 
 @Component({
   selector: 'app-create-client',
   templateUrl: './create-client.component.html',
   styleUrls: ['./create-client.component.css']
 })
+
+
 export class CreateClientComponent implements OnInit {
 
+
   model: FormModel = new FormModel();
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
 
+  createClient() {
+    if (this.model.clientName == "") {
+      alert("No client name typed!")
+      return
+    }
+    if (this.model.clientAddress == "") {
+      alert("No address typed!")
+      return
+    }
+    if (this.model.clientNif.length != 9) {
+      alert("NIF does not have correct number of digits!")
+      return
+    }
+    if (this.model.phoneNumber.length != 9) {
+      alert("Phone Number does not have correct number of digits!")
+      return
+    }
+
+
+    this.http.post<any>('http://localhost:8080/addclient', this.model, httpOptions).subscribe(data => {
+      location.reload();
+    });
+
+  }
 
 
 }
 
 export class FormModel{
-  clientNameValue: string = "";
-  nifValue: string = "";
-  clientAddressValue: string = "";
-  phoneNumberValue:string = "";
+  clientName: string = "";
+  clientNif: string = "";
+  clientAddress: string = "";
+  phoneNumber:string = "";
 }
